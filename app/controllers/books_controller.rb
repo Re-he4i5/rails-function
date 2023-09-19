896d1ee1c5c4b31b8af89c0439860bc504a229b6
Rehe-4i5 <kuronekoryohei@gmail.com>
2023-09-19 14:21:56 +0900

    検索機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,28 @@
 class BooksController < ApplicationController
+  def index
+    key_word = params[:keyword]
+    @books = Book.search(key_word)
+    @book = Book.new
+
+  end
+
+  def show
+    @book = Book.find params[:id]
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,10 @@
 class Book < ApplicationRecord
+
+  def self.search(keyword)
+    if keyword == ''
+      books = Book.all
+    else
+      books = Book.where(['title LIKE(?) OR body LIKE(?)', "%#{keyword}%", "%#{keyword}%"])
+    end
+  end
 end
