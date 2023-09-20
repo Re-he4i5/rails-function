ccc29f28915d419c48f5becd224384955e14dacd
Rehe-4i5 <kuronekoryohei@gmail.com>
2023-09-20 17:01:00 +0900

    楽天API&kaminariを用いたページング

--- Gemfile
+++ Gemfile
@@ -57,3 +57,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'rakuten_web_service'
 gem 'dotenv-rails'
+gem 'kaminari','~> 1.2.1'
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,18 +1,21 @@
 class HomesController < ApplicationController
   def top
-    @items = RakutenWebService::Ichiba::Item.search(keyword: "Rails")
-    render json: {items: @items}
-  end
-
-  def search
-    keyword = params[:keyword]
-    if keyword.size < 2
-      flash[:notice] = "2文字以上入力してください"
-      @items = RakutenWebService::Ichiba::Item.search(keyword: "Rails")
-      render 'top'
+    if params[:keyword]
+      if params[:keyword].size < 2
+        flash[:danger] = "検索ワードは2文字以上で入力してください"
+        render :top
+      else
+        items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword])
+        @items_full = []
+        items.each do |item|
+            @items_full.push(item)        
+        end
+        if @items_full.present?
+          @items = Kaminari.paginate_array(@items_full).page(params[:page])
+        end
+      end
     else
-      @items = RakutenWebService::Ichiba::Item.search(keyword: keyword)
-      render 'top'
+      @items = []
     end
   end
 
